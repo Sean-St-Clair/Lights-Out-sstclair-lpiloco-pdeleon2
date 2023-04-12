0fb3f0e41f87c12e56117b3da8466f7f4615fb88
Sean-300 <sstclair@uvm.edu>
2023-04-12 18:30:06 -0400

    Removed most graphics from runner project

--- graphics.cpp
+++ graphics.cpp
@@ -9,75 +9,13 @@ using namespace std;
 
 GLdouble width, height;
 int wd;
-const color skyBlue(77 / 255.0, 213 / 255.0, 240 / 255.0);
-const color grassGreen(26 / 255.0, 176 / 255.0, 56 / 255.0);
 const color white(1, 1, 1);
-const color brickRed(201 / 255.0, 20 / 255.0, 20 / 255.0);
-const color darkBlue(1 / 255.0, 110 / 255.0, 214 / 255.0);
-const color purple(119 / 255.0, 11 / 255.0, 224 / 255.0);
-const color black(0, 0, 0);
-const color magenta(1, 0, 1);
-const color orange(1, 163 / 255.0, 22 / 255.0);
-const color cyan(0, 1, 1);
 
 Rect grass;
 vector<Rect> buildings1;
 vector<Rect> buildings2;
 vector<Rect> buildings3;
 Rect user;
-
-void initGrass() {
-    grass.setCenter(250, 450);
-    grass.setSize(width, height / 3);
-    grass.setColor(grassGreen);
-}
-
-void initBuildings() {
-    // First vector is for closest buildings
-    int totalBuildingWidth = 0;
-    dimensions buildingSize;
-    while (totalBuildingWidth < width + 50) {
-        // Building height between 50-100
-        buildingSize.height = rand() % 51 + 50;
-        // Building width between 30-50
-        buildingSize.width = rand() % 21 + 30;
-        buildings1.push_back(Rect(brickRed,
-                                  totalBuildingWidth + (buildingSize.width / 2) + 5,
-                                  height - ((buildingSize.height / 2) + 50),
-                                  buildingSize));
-        totalBuildingWidth += buildingSize.width + 5;
-    }
-
-    // Populate second set of buildings
-    totalBuildingWidth = 0;
-    while (totalBuildingWidth < width + 100) {
-        // Populate this vector of darkBlue buildings
-        // Building height between 100-200
-        buildingSize.height = rand() % 101 + 100;
-        // Building width between 50-100
-        buildingSize.width = rand() % 51 + 50;
-        buildings2.push_back(Rect(darkBlue,
-                                  totalBuildingWidth + (buildingSize.width / 2) + 5,
-                                  height - ((buildingSize.height / 2) + 50),
-                                  buildingSize));
-        totalBuildingWidth += buildingSize.width + 5; // Placeholder line to avoid infinite loop
-    }
-
-    // Populate third set of buildings
-    totalBuildingWidth = 0;
-    while (totalBuildingWidth < width + 200) {
-        // Populate this vector of purple buildings
-        // Building height between 200-400
-        buildingSize.height = rand() % 201 + 200;
-        // Building width between 100-200
-        buildingSize.width = rand() % 101 + 100;
-        buildings3.push_back(Rect(purple,
-                                  totalBuildingWidth + (buildingSize.width / 2) + 5,
-                                  height - ((buildingSize.height / 2) + 50),
-                                  buildingSize));
-        totalBuildingWidth += buildingSize.width + 5; // Placeholder line to avoid infinite loop
-    }
-}
 
 void initUser() {
     // Initialize the user to be a 20x20 white block
@@ -90,15 +28,13 @@ void init() {
     width = 500;
     height = 500;
     srand(time(0));
-    initGrass();
-    initBuildings();
     initUser();
 }
 
 /* Initialize OpenGL Graphics */
 void initGL() {
     // Set "clearing" or background color
-    glClearColor(skyBlue.red, skyBlue.green, skyBlue.blue, 1.0f);
+    glClearColor(0, 0, 0, 1.0f);
 }
 
 /* Handler for window-repaint event. Call back when the window first appears and
@@ -121,41 +57,6 @@ void display() {
     /*
      * Draw here
      */
-
-    // Add logic to draw the grass, the user, and the
-    // rest of the buildings. Note that the order of drawing
-    // matters because whatever is drawn last appears on top.
-    // Note that darkBlue buildings turn cyan when overlapping
-    // with the user, and purple buildings turn magenta.
-
-    grass.draw();
-
-    for (Rect &r: buildings3) {
-        if (r.isOverlapping(user)) {
-            r.setColor(magenta);
-        } else {
-            r.setColor(purple);
-        }
-        r.draw();
-    }
-
-    for (Rect &r: buildings2) {
-        if (r.isOverlapping(user)) {
-            r.setColor(cyan);
-        } else {
-            r.setColor(darkBlue);
-        }
-        r.draw();
-    }
-
-    for (Rect &r: buildings1) {
-        if (r.isOverlapping(user)) {
-            r.setColor(orange);
-        } else {
-            r.setColor(brickRed);
-        }
-        r.draw();
-    }
 
     user.draw();
 
